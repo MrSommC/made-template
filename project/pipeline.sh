6b41c460746c9941928b7da4aaf04c96bc0e13d3
MrSommC <cc.sommermann@googlemail.com>
2024-11-13 14:39:34 +0100

    updated shell

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-
-# Ensure the script stops if any command fails
 set -e
 
-# Install required packages
 pip install -r requirements.txt
 
-# Run the main pipeline script
 python3 pipeline.py
